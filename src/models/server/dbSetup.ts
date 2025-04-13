ef0d85a8fb398d697a5664d848a01f7a952fbf31
Moksh Malde <maldemoksh437@gmail.com>
2025-04-13 21:17:58 +0530

    Implement database setup and collection creation logic

---
+++ src/models/server/dbSetup.ts
@@ -0,0 +1,34 @@
+import { db } from "../name";
+import createAnswerCollection from "./answer.collection";
+import createCommentCollection from "./comments.collection";
+import createQuestionCollection from "./questions.collection";
+import createVoteCollection from "./vote.collection";
+
+import { databases } from "./config";
+
+
+export default async function getOrCreateDB(){
+    try {
+      await databases.get(db)
+      console.log("Database connection")
+    } catch (error) {
+      try {
+        await databases.create(db, db)
+        console.log("database created")
+        //create collections
+        await Promise.all([
+          createQuestionCollection(),
+          createAnswerCollection(),
+          createCommentCollection(),
+          createVoteCollection(),
+  
+        ])
+        console.log("Collection created")
+        console.log("Database connected")
+      } catch (error) {
+        console.log("Error creating databases or collection", error)
+      }
+    }
+  
+    return databases
+  }
